2945d70da1b0707323b7e5826607ad757d431a25
demogorgon1 <neckelmann@gmail.com>
2023-01-11 13:26:46 +0100

    - Commented out some default test settings that got erroneously committed.

--- test/Main.cpp
+++ test/Main.cpp
@@ -11,17 +11,17 @@ main(
 	jelly::Test::Config config;
 	config.InitFromCommandLine(aNumArgs, aArgs);
 
-	config.m_simTest = true;
-	config.m_simCSVOutput = "test.csv";
-	config.m_simCSVOutputColumns = { "C_CONNECTED", "C_WAITING_FOR_CONNECTION", "C_WAITING_FOR_CONNECTION_cur_avg", "G_WAITING_FOR_BLOB_SET_cur_avg" };
-	config.m_simNumClients = 200000;
-	config.m_simNumClientThreads = 4;
-	config.m_simSetIntervalMS = 60 * 1000;
-	config.m_simClientStartIntervalMS = 1;
-	config.m_simNumBlobServers = 4;
-	config.m_simNumBlobServerThreads = 4;
-	config.m_simNumGameServers = 4;
-	config.m_simNumGameServerThreads = 4;
+	//config.m_simTest = true;
+	//config.m_simCSVOutput = "test.csv";
+	//config.m_simCSVOutputColumns = { "C_CONNECTED", "C_WAITING_FOR_CONNECTION", "C_WAITING_FOR_CONNECTION_cur_avg", "G_WAITING_FOR_BLOB_SET_cur_avg" };
+	//config.m_simNumClients = 200000;
+	//config.m_simNumClientThreads = 4;
+	//config.m_simSetIntervalMS = 60 * 1000;
+	//config.m_simClientStartIntervalMS = 1;
+	//config.m_simNumBlobServers = 4;
+	//config.m_simNumBlobServerThreads = 4;
+	//config.m_simNumGameServers = 4;
+	//config.m_simNumGameServerThreads = 4;
 
 	jelly::Test::AllTests::Run(".", &config);
 

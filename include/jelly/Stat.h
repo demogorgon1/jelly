b6eb705967a541d7f3e2c7952b5bac1c7a3f10b2
demogorgon1 <neckelmann@gmail.com>
2023-02-03 09:51:10 +0100

    - const stats info.

--- include/jelly/Stat.h
+++ include/jelly/Stat.h
@@ -63,44 +63,45 @@ namespace jelly
 		//! Information about a statistic
 		struct Info
 		{
-			Type			m_type;
-			const char*		m_id;
-			uint32_t		m_counterRateMovingAverage;
+			Type					m_type;
+			const char*				m_id;
+			uint32_t				m_counterRateMovingAverage;
+			std::vector<uint64_t>	m_samplerHistogramBuckets;
 		};
 
 		// IMPORTANT: Order must match the Counter enum
-		static constexpr Info INFO[] =
+		static const Info INFO[] =
 		{
-			//                                          | m_type       | m_id                               | m_counterRateMovingAverage
-			//------------------------------------------+--------------+------------------------------------+-----------------------------
-			/* ID_DISK_WRITE_LOCK_WAL_BYTES */		    { TYPE_COUNTER, "disk_write_lock_wal_bytes",          10 },
-			/* ID_DISK_WRITE_LOCK_STORE_BYTES */	    { TYPE_COUNTER, "disk_write_lock_store_bytes",        10 },
-			/* ID_DISK_WRITE_BLOB_WAL_BYTES */		    { TYPE_COUNTER, "disk_write_blob_wal_bytes",          10 },
-			/* ID_DISK_WRITE_BLOB_STORE_BYTES */	    { TYPE_COUNTER, "disk_write_blob_store_bytes",        10 },
-			/* ID_DISK_READ_LOCK_WAL_BYTES */		    { TYPE_COUNTER, "disk_read_lock_wal_bytes",           10 },
-			/* ID_DISK_READ_LOCK_STORE_BYTES */		    { TYPE_COUNTER, "disk_read_lock_store_bytes",         10 },
-			/* ID_DISK_READ_BLOB_WAL_BYTES */		    { TYPE_COUNTER, "disk_read_blob_wal_bytes",           10 },
-			/* ID_DISK_READ_BLOB_STORE_BYTES */         { TYPE_COUNTER, "disk_read_blob_store_bytes",         10 },
-			/* ID_FLUSH_PENDING_LOCK_WAL_TIME */	    { TYPE_SAMPLER, "flush_pending_lock_wal_time",        0 },
-			/* ID_FLUSH_PENDING_BLOB_WAL_TIME */	    { TYPE_SAMPLER, "flush_pending_blob_wal_time",        0 },
-			/* ID_PROCESS_LOCK_REQUESTS_TIME */         { TYPE_SAMPLER, "process_lock_requests_time",         0 },
-			/* ID_PROCESS_BLOB_REQUESTS_TIME */         { TYPE_SAMPLER, "process_blob_requests_time",         0 },
-			/* ID_FLUSH_PENDING_LOCK_STORE_TIME */      { TYPE_SAMPLER, "flush_pending_lock_store_time",      0 },
-			/* ID_FLUSH_PENDING_BLOB_STORE_TIME */      { TYPE_SAMPLER, "flush_pending_blob_store_time",      0 },
-			/* ID_CLEANUP_LOCK_WALS_TIME */             { TYPE_SAMPLER, "cleanup_lock_wals_time",             0 },
-			/* ID_CLEANUP_BLOB_WALS_TIME */             { TYPE_SAMPLER, "cleanup_blob_wals_time",             0 },
-			/* ID_PERFORM_LOCK_COMPACTION_TIME */       { TYPE_SAMPLER, "perform_lock_compaction_time",       0 },
-			/* ID_PERFORM_BLOB_COMPACTION_TIME */       { TYPE_SAMPLER, "perform_blob_compaction_time",       0 },
-			/* ID_PERFORM_MAJOR_LOCK_COMPACTION_TIME */ { TYPE_SAMPLER, "perform_major_lock_compaction_time", 0 },
-			/* ID_PERFORM_MAJOR_BLOB_COMPACTION_TIME */ { TYPE_SAMPLER, "perform_major_blob_compaction_time", 0 },
-			/* ID_APPLY_LOCK_COMPACTION_TIME */         { TYPE_SAMPLER, "apply_lock_compaction_time",         0 },
-			/* ID_APPLY_BLOB_COMPACTION_TIME */         { TYPE_SAMPLER, "apply_blob_compaction_time",         0 },
-			/* ID_LOCK_WAL_COUNT */                     { TYPE_GAUGE,   "lock_wal_count",                     0 },
-			/* ID_BLOB_WAL_COUNT */                     { TYPE_GAUGE,   "blob_wal_count",                     0 },
-			/* ID_STATS_UPDATE_TIME */				    { TYPE_SAMPLER, "stats_update_time",                  0 },
-			/* ID_OBEY_RESIDENT_BLOB_LIMITS_TIME */		{ TYPE_SAMPLER, "obey_resident_blob_limits_time",     0 },
-			/* ID_TOTAL_RESIDENT_BLOB_SIZE */           { TYPE_GAUGE,   "total_resident_blob_size",           0 },
-			/* ID_TOTAL_RESIDENT_BLOB_COUNT */          { TYPE_GAUGE,   "total_resident_blob_count",          0 },
+			//                                          | m_type       | m_id                               | m_counterRateMovingAverage | m_samplerHistogramBuckets
+			//------------------------------------------+--------------+------------------------------------+----------------------------+---
+			/* ID_DISK_WRITE_LOCK_WAL_BYTES */		    { TYPE_COUNTER, "disk_write_lock_wal_bytes",          10,						   {} },
+			/* ID_DISK_WRITE_LOCK_STORE_BYTES */	    { TYPE_COUNTER, "disk_write_lock_store_bytes",        10,						   {} },
+			/* ID_DISK_WRITE_BLOB_WAL_BYTES */		    { TYPE_COUNTER, "disk_write_blob_wal_bytes",          10,						   {} },
+			/* ID_DISK_WRITE_BLOB_STORE_BYTES */	    { TYPE_COUNTER, "disk_write_blob_store_bytes",        10,						   {} },
+			/* ID_DISK_READ_LOCK_WAL_BYTES */		    { TYPE_COUNTER, "disk_read_lock_wal_bytes",           10,						   {} },
+			/* ID_DISK_READ_LOCK_STORE_BYTES */		    { TYPE_COUNTER, "disk_read_lock_store_bytes",         10,						   {} },
+			/* ID_DISK_READ_BLOB_WAL_BYTES */		    { TYPE_COUNTER, "disk_read_blob_wal_bytes",           10,						   {} },
+			/* ID_DISK_READ_BLOB_STORE_BYTES */         { TYPE_COUNTER, "disk_read_blob_store_bytes",         10,						   {} },
+			/* ID_FLUSH_PENDING_LOCK_WAL_TIME */	    { TYPE_SAMPLER, "flush_pending_lock_wal_time",        0,						   {} },
+			/* ID_FLUSH_PENDING_BLOB_WAL_TIME */	    { TYPE_SAMPLER, "flush_pending_blob_wal_time",        0,						   {} },
+			/* ID_PROCESS_LOCK_REQUESTS_TIME */         { TYPE_SAMPLER, "process_lock_requests_time",         0,						   {} },
+			/* ID_PROCESS_BLOB_REQUESTS_TIME */         { TYPE_SAMPLER, "process_blob_requests_time",         0,						   {} },
+			/* ID_FLUSH_PENDING_LOCK_STORE_TIME */      { TYPE_SAMPLER, "flush_pending_lock_store_time",      0,						   {} },
+			/* ID_FLUSH_PENDING_BLOB_STORE_TIME */      { TYPE_SAMPLER, "flush_pending_blob_store_time",      0,						   {} },
+			/* ID_CLEANUP_LOCK_WALS_TIME */             { TYPE_SAMPLER, "cleanup_lock_wals_time",             0,						   {} },
+			/* ID_CLEANUP_BLOB_WALS_TIME */             { TYPE_SAMPLER, "cleanup_blob_wals_time",             0,						   {} },
+			/* ID_PERFORM_LOCK_COMPACTION_TIME */       { TYPE_SAMPLER, "perform_lock_compaction_time",       0,						   {} },
+			/* ID_PERFORM_BLOB_COMPACTION_TIME */       { TYPE_SAMPLER, "perform_blob_compaction_time",       0,						   {} },
+			/* ID_PERFORM_MAJOR_LOCK_COMPACTION_TIME */ { TYPE_SAMPLER, "perform_major_lock_compaction_time", 0,						   {} },
+			/* ID_PERFORM_MAJOR_BLOB_COMPACTION_TIME */ { TYPE_SAMPLER, "perform_major_blob_compaction_time", 0,						   {} },
+			/* ID_APPLY_LOCK_COMPACTION_TIME */         { TYPE_SAMPLER, "apply_lock_compaction_time",         0,						   {} },
+			/* ID_APPLY_BLOB_COMPACTION_TIME */         { TYPE_SAMPLER, "apply_blob_compaction_time",         0,						   {} },
+			/* ID_LOCK_WAL_COUNT */                     { TYPE_GAUGE,   "lock_wal_count",                     0,						   {} },
+			/* ID_BLOB_WAL_COUNT */                     { TYPE_GAUGE,   "blob_wal_count",                     0,						   {} },
+			/* ID_STATS_UPDATE_TIME */				    { TYPE_SAMPLER, "stats_update_time",                  0,						   {} },
+			/* ID_OBEY_RESIDENT_BLOB_LIMITS_TIME */		{ TYPE_SAMPLER, "obey_resident_blob_limits_time",     0,						   {} },
+			/* ID_TOTAL_RESIDENT_BLOB_SIZE */           { TYPE_GAUGE,   "total_resident_blob_size",           0,						   {} },
+			/* ID_TOTAL_RESIDENT_BLOB_COUNT */          { TYPE_GAUGE,   "total_resident_blob_count",          0,						   {} },
 		};
 		
 		static_assert(sizeof(INFO) == sizeof(Info) * (size_t)NUM_IDS);
